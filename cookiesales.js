3be30f3fec1b28f212e66f767515b65e2e35454f
Oscar Cauich <ocauich@gravitypayments.com>
2017-04-10 15:24:18 -0700

    Created all objects for each store

--- cookiesales.js
+++ cookiesales.js
@@ -26,6 +26,42 @@ var SeaTacAirport = {
   locationName: '1st and PIke',
 }
 
+var SeattleCenter = {
+  ownersName: 'Michael Wood',
+  address: '1100 Sunset Bldv NE',
+  city:'Renton',
+  state: 'WA',
+  zipCode: 98056,
+  phoneNumber: 2063885908,
+  hourlyMinCustomer: 11,
+  hourlyMaxCustomer: 38,
+  avergHourlyCookieSales:3.7,
+  locationName: '1st and PIke',
+}
+var CapitolHill = {
+  ownersName: 'Michael Wood',
+  address: '1100 Sunset Bldv NE',
+  city:'Renton',
+  state: 'WA',
+  zipCode: 98056,
+  phoneNumber: 2063885908,
+  hourlyMinCustomer: 20,
+  hourlyMaxCustomer: 38,
+  avergHourlyCookieSales:2.3,
+  locationName: '1st and PIke',
+}
+var Alki = {
+  ownersName: 'Michael Wood',
+  address: '1100 Sunset Bldv NE',
+  city:'Renton',
+  state: 'WA',
+  zipCode: 98056,
+  phoneNumber: 2063885908,
+  hourlyMinCustomer: 2,
+  hourlyMaxCustomer: 16,
+  avergHourlyCookieSales:4.6,
+  locationName: '1st and PIke',
+}
 // var heading = document.getElementById('title');
 // heading.textContent = firstAndPike.ownersName;
 var storesTable = document.getElementById('storelistName');
